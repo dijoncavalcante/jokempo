c386b8305eec2453c5a2cb071b8bf47b32c8f7c1
dijoncavalcante <dijoncavalcante@gmail.com>
2021-02-10 18:52:31 -0400

    Create event touch, and add findviewbyids to buttons

--- app/src/main/java/com/dijon/jokempo/MainActivity.java
+++ app/src/main/java/com/dijon/jokempo/MainActivity.java
@@ -3,12 +3,32 @@ package com.dijon.jokempo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView jogador1;
+    ImageView jogador2;
+    ImageButton botaoPedra;
+    ImageButton botaoPapel;
+    ImageButton botaoTesura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        jogador1 = findViewById(R.id.jogador1);
+        jogador2 = findViewById(R.id.jogador2);
+        botaoPedra = findViewById(R.id.botaoPedra);
+        botaoPapel = findViewById(R.id.botaoPapel);
+        botaoTesura = findViewById(R.id.botaoTesoura);
+    }
+
+
+    public void tocouBotao(View view) {
+
     }
 }
